533e01a870f7a58d0a23f7caee2f4abfb843a585
alad1nks <vasya.sidr777@gmail.com>
2023-12-19 23:15:14 +0300

    init

---
+++ src/test/kotlin/com/alad1nks/dubovozkibackend/DubovozkiBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alad1nks.dubovozkibackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DubovozkiBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
