54be029ba628107b55f54f69479568f544ff3d59
alad1nks <vasya.sidr777@gmail.com>
2024-01-03 12:15:23 +0300

    feat bus-schedule: rename busEntityList to busList

--- src/main/kotlin/com/alad1nks/dubovozkibackend/bus_schedule/entities/BusSchedule.kt
+++ src/main/kotlin/com/alad1nks/dubovozkibackend/bus_schedule/entities/BusSchedule.kt
@@ -1,6 +1,6 @@
 package com.alad1nks.dubovozkibackend.bus_schedule.entities
 
 data class BusSchedule(
-    val busEntityList: List<BusEntity>,
+    val busList: List<BusEntity>,
     val revision: Int
 )
