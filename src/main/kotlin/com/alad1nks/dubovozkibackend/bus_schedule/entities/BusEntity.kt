82b12b259ae5d9b560c65d58de43761ef5b78f62
alad1nks <vasya.sidr777@gmail.com>
2024-01-03 22:39:37 +0300

    feat bus-schedule: rollback to old sql schema

--- src/main/kotlin/com/alad1nks/dubovozkibackend/bus_schedule/entities/BusEntity.kt
+++ src/main/kotlin/com/alad1nks/dubovozkibackend/bus_schedule/entities/BusEntity.kt
@@ -2,14 +2,14 @@ package com.alad1nks.dubovozkibackend.bus_schedule.entities
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
-import java.sql.Time
 
 @Table("BUS_SCHEDULE")
 data class BusEntity(
     @Id
     var id: Int?,
     val dayOfWeek: Int,
-    val dayTime: Time,
+    val dayTime: Long,
+    val dayTimeString: String,
     val direction: String,
     val station: String
 )
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS bus_schedule (
-    id          IDENTITY        NOT NULL    PRIMARY KEY,
-    day_of_week INT             NOT NULL,
-    day_time    TIME            NOT NULL,
-    direction   VARCHAR(3)      NOT NULL,
-    station     VARCHAR(3)      NOT NULL
+    id                  IDENTITY        NOT NULL    PRIMARY KEY,
+    day_of_week         INT             NOT NULL,
+    day_time            BIGINT          NOT NULL,
+    day_time_string     VARCHAR(5)      NOT NULL,
+    direction           VARCHAR(3)      NOT NULL,
+    station             VARCHAR(3)      NOT NULL
 );
