63486aaedac7472af186651bd73feb5548d29d62
alad1nks <vasya.sidr777@gmail.com>
2024-02-12 23:10:37 +0300

    feat registration: set tokens update rate to 1 second

--- src/main/kotlin/com/alad1nks/dubovozkibackend/registration/services/TokenCleanupService.kt
+++ src/main/kotlin/com/alad1nks/dubovozkibackend/registration/services/TokenCleanupService.kt
@@ -8,7 +8,7 @@ import java.util.*
 @Component
 class TokenCleanupService(val tokensRepository: RegistrationTokensRepository) {
 
-    @Scheduled(fixedRate = 30000)
+    @Scheduled(fixedRate = 1000)
     fun deleteExpiredTokens() {
         val currentTime = Calendar.getInstance().timeInMillis
         tokensRepository.deleteByExpiryDateBefore(currentTime)
