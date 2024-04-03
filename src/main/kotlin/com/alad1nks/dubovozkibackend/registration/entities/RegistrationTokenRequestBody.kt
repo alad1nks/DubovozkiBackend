9cb2b3f1fc938543e0b58209ac7818fef97a73f8
alad1nks <vasya.sidr777@gmail.com>
2024-04-04 01:17:34 +0300

    feat registration: fix duplicating users

--- src/main/kotlin/com/alad1nks/dubovozkibackend/registration/entities/RegistrationTokenRequestBody.kt
+++ src/main/kotlin/com/alad1nks/dubovozkibackend/registration/entities/RegistrationTokenRequestBody.kt
@@ -2,5 +2,6 @@ package com.alad1nks.dubovozkibackend.registration.entities
 
 data class RegistrationTokenRequestBody(
     val email: String,
-    val token: String
+    val token: String,
+    val telegramId: String? = null
 )
