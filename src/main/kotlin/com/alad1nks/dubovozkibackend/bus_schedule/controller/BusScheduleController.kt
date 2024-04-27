ea02a2a7437d08efcfe05dd665eb139b30c24213
alad1nks <vasya.sidr777@gmail.com>
2024-04-27 16:04:02 +0300

    feat bus-schedule: update logic

--- src/main/kotlin/com/alad1nks/dubovozkibackend/bus_schedule/controller/BusScheduleController.kt
+++ src/main/kotlin/com/alad1nks/dubovozkibackend/bus_schedule/controller/BusScheduleController.kt
@@ -1,6 +1,9 @@
 package com.alad1nks.dubovozkibackend.bus_schedule.controller
 
 import com.alad1nks.dubovozkibackend.bus_schedule.entities.BusEntity
+import com.alad1nks.dubovozkibackend.bus_schedule.entities.BusScheduleResponse
+import com.alad1nks.dubovozkibackend.bus_schedule.entities.BusScheduleRevisionResponse
+import com.alad1nks.dubovozkibackend.bus_schedule.service.GetBusScheduleRevisionService
 import com.alad1nks.dubovozkibackend.bus_schedule.service.GetBusScheduleService
 import com.alad1nks.dubovozkibackend.bus_schedule.service.UpdateBusScheduleService
 import org.springframework.web.bind.annotation.*
@@ -9,13 +12,17 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/router/bus-schedule")
 class BusScheduleController(
     val getBusScheduleService: GetBusScheduleService,
+    val getBusScheduleRevisionService: GetBusScheduleRevisionService,
     val updateBusScheduleService: UpdateBusScheduleService
 ) {
-    @GetMapping
-    fun getBusSchedule() = getBusScheduleService()
+    @GetMapping("/list")
+    fun getBusSchedule(): BusScheduleResponse = getBusScheduleService()
+
+    @GetMapping("/revision")
+    fun getBusScheduleRevision(): BusScheduleRevisionResponse = getBusScheduleRevisionService()
 
     @PostMapping("/update")
     fun updateBusSchedule(
         @RequestBody busList: List<BusEntity>
-    ) = updateBusScheduleService(busList)
+    ): String = updateBusScheduleService(busList)
 }
---
+++ src/main/kotlin/com/alad1nks/dubovozkibackend/bus_schedule/entities/BusScheduleResponse.kt
@@ -0,0 +1,5 @@
+package com.alad1nks.dubovozkibackend.bus_schedule.entities
+
+data class BusScheduleResponse(
+    val busList: List<BusEntity>
+)
